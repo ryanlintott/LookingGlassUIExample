0b7a58aa5f7cd90331164d53d16658e48b9e30c3
Ryan Lintott <ryan.lintott@abetterwaytodo.com>
2021-05-18 10:22:17 -0400

    Initial Commit

---
+++ LookingGlassUIExample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LookingGlassUIExample
+//
+//  Created by Ryan Lintott on 2021-05-18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

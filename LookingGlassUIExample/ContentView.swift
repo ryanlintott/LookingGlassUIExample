f65de55061333a26c111091dfb0cd3d3762cedb5
Ryan Lintott <ryan.lintott@abetterwaytodo.com>
2021-05-18 10:56:46 -0400

    Changed updateInterval and disabled in MotionManagerViewModifier from Binding to let constants as they are never changed internally

--- LookingGlassUIExample/ContentView.swift
+++ LookingGlassUIExample/ContentView.swift
@@ -73,7 +73,7 @@ struct ContentView: View {
             
             SettingsView(updateInterval: $updateInterval, disabled: $disabled)
         }
-        .motionManager(updateInterval: $updateInterval, disabled: $disabled)
+        .motionManager(updateInterval: updateInterval, disabled: disabled)
     }
 }
 

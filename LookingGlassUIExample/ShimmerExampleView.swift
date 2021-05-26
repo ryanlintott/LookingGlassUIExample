7e347c98944db86b996053823ecf40e4ccede335
Ryan Lintott <ryan.lintott@abetterwaytodo.com>
2021-05-26 10:00:47 -0400

    Fixed a bug where shimmer would be on for darkModeOnly Reordered ShimmerMode enums and conformed to Int ShimmerExampleView now tests off, on, darkModeOnly and LightModeOnly

--- LookingGlassUIExample/ShimmerExampleView.swift
+++ LookingGlassUIExample/ShimmerExampleView.swift
@@ -5,28 +5,55 @@
 //  Created by Ryan Lintott on 2021-05-11.
 //
 
+import LookingGlassUI
 import SwiftUI
 
 struct ShimmerExampleView: View {
+    
+    func mode(number: Int) -> ShimmerMode {
+        switch number % 4 {
+        case 0:
+            return .off
+        case 1:
+            return .on
+        case 2:
+            return .darkModeOnly
+        default:
+            return .lightModeOnly
+        }
+    }
+    
+    func text(number: Int) -> String {
+        switch number % 4 {
+        case 0:
+            return "Gold \(number): OFF"
+        case 1:
+            return "Gold \(number): ON"
+        case 2:
+            return "Gold \(number): DarkOnly"
+        default:
+            return "Gold \(number): LightOnly"
+        }
+    }
+
     var body: some View {
         ScrollView {
             LazyVStack {
-                ForEach(1..<100, id: \.self) { i in
+                ForEach(0..<100, id: \.self) { i in
                     VStack {
                         Rectangle()
-                            .fill(Color.goldDark)
+                            .frame(height: 4)
                         
-                        Text("Gold Text \(i)")
-                            .font(.system(size: 50))
+                        Text(text(number: i))
+                            .font(.system(size: 48))
                             .fontWeight(.bold)
-                            .foregroundColor(.goldDark)
                             .padding()
                     }
-                    .shimmer(color: .goldShimmer, background: .goldDark)
+                    .foregroundColor(.goldDark)
+                    .shimmer(mode: mode(number: i), color: .goldShimmer)
                 }
             }
         }
-        .background(Color.black.edgesIgnoringSafeArea(.all))
     }
 }
 
